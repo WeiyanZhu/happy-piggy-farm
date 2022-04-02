3592f4ef0837941ef82c8eaade1b90cd4d2a0d37
Weiyan Zhu <weiyanzhu0724@gmail.com>
2022-04-02 16:38:04 -0700

    basic main menu

---
+++ Happy Piggy Farm/Assets/System/MainMenu/MainMenuManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private bool canControl = true;
+
+    void Start(){
+        //SystemManager.instance.AudioManager.PlayMusic(BGMFileName.MainMenu);
+    }
+
+    public void NewGameButton()
+    {
+        if(!canControl)
+            return;
+        canControl = false;
+
+        //SystemManager.instance.AudioManager.PlaySFX(SFXFileName.UIConfirm);
+        SceneManager.LoadScene("Game");
+    }
+
+    /*
+    [Header("Language")]
+    [SerializeField] private SetTextWithJSON[] mainMenuTexts;
+
+    public void ChangeLanguageButton()
+    {
+        SystemManager.instance.AudioManager.PlaySFX(SFXFileName.Interact);
+        int len = System.Enum.GetNames(typeof(Language)).Length;
+        //switch to the next language
+        Language newLan = (Language) (((int)(SystemManager.instance.Language) + 1)%len);
+        ChangeLanguage(newLan);
+    }
+
+    public void ChangeLanguage(Language lan)
+    {
+        if(SystemManager.instance.Language != lan)
+        {
+            SystemManager.instance.Language = lan;
+            foreach(SetTextWithJSON t in mainMenuTexts){
+                t.UpdateValue();
+            }
+        }
+    }*/
+}
---
+++ Happy Piggy Farm/Assets/System/Util/ObjectScaleAnimator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger))]
+public class ObjectScaleAnimator : MonoBehaviour
+{
+    [SerializeField] private Vector3 initSize = new Vector3(1f, 1f, 1f);
+    [SerializeField] private Vector3 maxSize = new Vector3(1.5f, 1.5f, 1.5f);
+    [SerializeField] private float scalingSpeed = 0.2f;
+    private bool mouseOnObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseOnObject = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = mouseOnObject?maxSize:initSize;
+        Vector3 current = transform.localScale;
+        if(Vector3.Distance(target, current) > 0.01f)
+        {
+            Vector3 newScale = Vector3.Lerp(current, target, scalingSpeed * 60 * Time.unscaledDeltaTime);
+            transform.localScale = newScale;
+        }
+    }
+
+    //needs to be called by EventTrigger component, set state when mouse enter object
+    public void OnMouseEnterObject()
+    {
+        //SystemManager.instance.AudioManager.PlaySFX(SFXFileName.UIHover);
+        mouseOnObject = true;
+    }
+
+    //needs to be called by EventTrigger component, set state when mouse enter object
+    public void OnMouseExitObject()
+    {
+        mouseOnObject = false;
+    }
+
+    //reset the state when disabled to solve issue: when going to another page and go back the button remain selected
+    void OnDisable()
+    {
+        mouseOnObject = false;
+        transform.localScale = initSize;
+    }
+}
