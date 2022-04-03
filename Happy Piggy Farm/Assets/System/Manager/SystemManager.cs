917888802b07de774012b33c7b5fd3639cceff41
Weiyan Zhu <weiyanzhu0724@gmail.com>
2022-04-03 10:12:45 -0700

    remove testing code && set a day to 60s

--- Happy Piggy Farm/Assets/System/Manager/SystemManager.cs
+++ Happy Piggy Farm/Assets/System/Manager/SystemManager.cs
@@ -15,7 +15,6 @@ public class SystemManager : Singleton<SystemManager>
         {
             SystemManager.Instance.NewGamePlus = true;
         }
-        SystemManager.Instance.NewGamePlus = true;
     }
 
     void Update()
